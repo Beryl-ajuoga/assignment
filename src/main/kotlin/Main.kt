7f955ba539641f958ef3ef7714cfec46d136874f
beryl-ajuoga <berrylajuoga@gmail.com>
2022-02-16 21:48:15 +0300

    my work

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var name="beryl akinyi"
+    println(name)
+
+    var age ="21"
+    println(age)
+
+    val phoneNumber ="0712300022"
+    println(phoneNumber)
+
+    var weight = "72"
+    println(weight)
+
+    var kenyanCitizen =true
+    println(kenyanCitizen)
+}
